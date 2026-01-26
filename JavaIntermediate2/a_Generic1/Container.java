1ebf11e7e66ae37f102927b65e2a02cd132842e8
daniel <hun320daniel07@gmail.com>
2026-01-26 15:22:10 +0900

    Java Intermediate 2 Generic 실습코드 1 추가

---
+++ JavaIntermediate2/a_Generic1/Container.java
@@ -0,0 +1,17 @@
+package a_Generic1;
+
+public class Container<T> {
+    private T value;
+
+    public void setItem(T value) {
+        this.value = value;
+    }
+
+    public T getItem() {
+        return value;
+    }
+
+    public boolean isEmpty() {
+        return value == null;
+    }
+}
